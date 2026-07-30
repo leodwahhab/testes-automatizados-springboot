454ac7bd0fe2a0e8644902bca6b51c57f5872098
leonardo wahhab <leonardo.wahhab@gmail.com>
2026-07-29 23:45:43 -0300

    aula 36 e 37: add integration tests for Planet; update datasource configuration and add SQL script for planet removal

---
+++ src/test/java/com/example/sw_planet_api/PlanetIT.java
@@ -0,0 +1,34 @@
+package com.example.sw_planet_api;
+
+import com.example.sw_planet_api.domain.Planet;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import static com.example.sw_planet_api.commons.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("it")
+@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = {"/remove_planets.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class PlanetIT {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void createPlanet_returnsCreated() {
+        ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(sut.getBody().getId()).isNotNull();
+        assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+
+}
